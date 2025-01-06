6352590d4a5279aaf6490ebffea76770aff76a27
matt styles <styles.matt135@gmail.com>
2025-01-06 15:41:56 -0500

    Contact Page Functionality

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,33 +1,47 @@
-import React from 'react'
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 export default function ContactPage() {
+
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [comments, setComments] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Firstname:", firstname);
+    console.log("Lastname:", lastname);
+    console.log("Email:", email);
+    console.log("Comments:", comments);
+  };
+
   return (
     <>
     <h1>Contact Page</h1>
-      <Form>
-        <Form.Group className="mb-3">
+        <Form onSubmit={handleSubmit}>
+        <Form.Group className="first-name">
          <Form.Label>First Name</Form.Label>
-          <Form.Control type="text" placeholder="ex: John" />
+          <Form.Control type="text" value={firstname} onChange={(e) => setFirstName(e.target.value)}placeholder="ex: John" />
           <Form.Text className="text-muted">
           </Form.Text>
         </Form.Group>
-        <Form.Group className="mb-3">
+        <Form.Group className="last-name">
          <Form.Label>Last Name</Form.Label>
-          <Form.Control type="text" placeholder="ex: Doe" />
+          <Form.Control type="text" value={lastname} onChange={(e) => setLastName(e.target.value)} placeholder="ex: Doe" />
           <Form.Text className="text-muted">
           </Form.Text>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className="email" controlId="formBasicEmail">
          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="ex: johndoe@email.com" />
+          <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="ex: johndoe@email.com" />
           <Form.Text className="text-muted">
           </Form.Text>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Group className="comments" controlId="exampleForm.ControlTextarea1">
         <Form.Label>Comments</Form.Label>
-        <Form.Control as="textarea" rows={3} />
+        <Form.Control as="textarea" rows={3} value={comments} onChange={(e) => setComments(e.target.value)}/>
       </Form.Group>
         <Button variant="primary" type="submit">
           Submit
